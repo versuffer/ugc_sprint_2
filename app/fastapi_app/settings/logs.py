43786e236c3b7e4ff17923213693993528d79cbf
Яна Бубнова <bujhvh@mail.ru>
2024-09-08 22:33:27 +0300

    json logs

--- app/fastapi_app/settings/logs.py
+++ app/fastapi_app/settings/logs.py
@@ -19,12 +19,23 @@ logger_config = {
             'fmt': '%(levelname)s: [%(asctime)s - %(filename)s:%(lineno)s - %(funcName)s] %(message)s',
             'datefmt': '%Y-%m-%d %H:%M:%S',
         },
+        'json': {
+            '()': 'app.fastapi_app.settings.logging_utils.JSONFormatter',
+            'fmt_keys': {
+                'level': 'levelname',
+                'timestamp': 'timestamp',
+                'message': 'message',
+                'module': 'module',
+                'function': 'funcName',
+                'line': 'lineno',
+            },
+        },
     },
     'handlers': {
         'console': {
             'level': settings.LOG_LEVEL,
-            'formatter': 'default',
-            'class': 'logging.StreamHandler',
+            'formatter': 'json' if settings.JSON_LOGS is True else 'default',
+            'class': 'app.fastapi_app.settings.logging_utils.RequestIdStreamHandler',
             'stream': 'ext://sys.stderr',
         },
         'file': {
