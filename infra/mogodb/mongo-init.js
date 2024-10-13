754adbdb0ceddb8e291786b7c071870f955207b0
Dmitrii Annsjaw <rotanin@gmail.com>
2024-10-13 16:08:28 +0500

    add compose db

---
+++ infra/mogodb/mongo-init.js
@@ -0,0 +1,5 @@
+db = db.getSiblingDB('MoviesDB');
+db.createCollection('BookmarkModel');
+db.createCollection('ScoreModel');
+db.createCollection('TextReviewModel');
+db.createCollection('ScoreReviewModel');
